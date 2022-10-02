ed8f0db5c1da92903fc3e0f14f27663a18a148ad
nadams019 <nadams019@users.noreply.github.com>
2022-10-02 14:12:53 -0400

    Initial commit

---
+++ makefile
@@ -0,0 +1,22 @@
+API_DIR = server
+DB_DIR = db
+REQ_DIR = .
+
+FORCE:
+
+prod: all_tests github
+
+github: FORCE
+	- git commit -a
+	git push origin master
+
+all_tests: FORCE
+	cd $(DB_DIR); make tests
+	cd $(API_DIR); make tests
+
+dev_env: FORCE
+	pip install -r $(REQ_DIR)/requirements-dev.txt
+
+docs: FORCE
+	cd $(API_DIR); make docs
+	cd $(DB_DIR); make docs
---
+++ server/endpoints.py
@@ -0,0 +1,95 @@
+"""
+This is the file containing all of the endpoints for our flask app.
+The endpoint called `endpoints` will return all available endpoints.
+"""
+from http import HTTPStatus
+
+from flask import Flask
+from flask_restx import Resource, Api
+import werkzeug.exceptions as wz
+
+import db.char_types as ctyp
+
+app = Flask(__name__)
+api = Api(app)
+
+LIST = 'list'
+DETAILS = 'details'
+MAIN_MENU = '/main_menu'
+MAIN_MENU_NM = 'Main Menu'
+HELLO = '/hello'
+MESSAGE = 'message'
+CHAR_TYPE_LIST = f'/character_types/{LIST}'
+CHAR_TYPE_LIST_NM = 'character_types_list'
+CHAR_TYPE_DETAILS = f'/character_types/{DETAILS}'
+
+
+@api.route(HELLO)
+class HelloWorld(Resource):
+    """
+    The purpose of the HelloWorld class is to have a simple test to see if the
+    app is working at all.
+    """
+    def get(self):
+        """
+        A trivial endpoint to see if the server is running.
+        It just answers with "hello world."
+        """
+        return {MESSAGE: 'hello world'}
+
+
+@api.route(MAIN_MENU)
+class MainMenu(Resource):
+    """
+    This will deliver our main menu.
+    """
+    def get(self):
+        """
+        Gets the main game menu.
+        """
+        return {MAIN_MENU_NM: {'the': 'menu'}}
+
+
+@api.route(CHAR_TYPE_LIST)
+class CharacterTypeList(Resource):
+    """
+    This will get a list of character types.
+    """
+    def get(self):
+        """
+        Returns a list of character types.
+        """
+        return {CHAR_TYPE_LIST_NM: ctyp.get_char_types()}
+
+
+@api.route(f'{CHAR_TYPE_DETAILS}/<char_type>')
+class CharacterTypeDetails(Resource):
+    """
+    This will get a list of character types.
+    """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
+    def get(self, char_type):
+        """
+        Returns a list of character types.
+        """
+        ct = ctyp.get_char_type_details(char_type)
+        if ct is not None:
+            return {char_type: ctyp.get_char_type_details(char_type)}
+        else:
+            raise wz.NotFound(f'{char_type} not found.')
+
+
+@api.route('/endpoints')
+class Endpoints(Resource):
+    """
+    This class will serve as live, fetchable documentation of what endpoints
+    are available in the system.
+    """
+    def get(self):
+        """
+        The `get()` method will return a list of available endpoints.
+        """
+        endpoints = ''
+        # sorted(rule.rule for rule in api.app.url_map.iter_rules())
+        return {"Available endpoints": endpoints}
